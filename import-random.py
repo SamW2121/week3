26822158b73b0afc06778837fb99ec4812911616
SamW2121 <110577253+SamW2121@users.noreply.github.com>
2022-08-04 10:42:49 +0800

    s

--- import-random.py
+++ import-random.py
@@ -2,7 +2,13 @@ import random
 
 password = ""
 
-for i in range(10):
+
+while True:
+    password_length = int(input('> '))
+    if 0 < password_length < 50:
+        break
+
+for i in range(password_length):
     random_integer = random.randint(0,126)
     password += chr(random_integer)
 print(password)
